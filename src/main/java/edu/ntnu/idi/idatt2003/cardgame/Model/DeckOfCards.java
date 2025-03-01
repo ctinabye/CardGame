796d165767cc722f79e17a60b75a9993e55185b3
Christina <chrisnby@stud.ntnu.no>
2025-03-01 13:50:28 +0100

    feat(DeckOfCards): add constructor

---
+++ src/main/java/edu/ntnu/idi/idatt2003/cardgame/Model/DeckOfCards.java
@@ -0,0 +1,30 @@
+package edu.ntnu.idi.idatt2003.cardgame.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a deck of cards.
+ *
+ * @version 1.0
+ */
+public class DeckOfCards {
+  private static final char[] suit = {'S', 'H', 'D', 'C'};
+  private List<PlayingCard> cards;
+
+  /**
+   * Constructs 52 cards with one of each combination of suit and face value.
+   */
+  public DeckOfCards() {
+    this.cards = new ArrayList<PlayingCard>();
+
+    for (char suit : suit) {
+      for (int face = 1; face <= 13; face++) {
+        cards.add(new PlayingCard(suit, face));
+      }
+    }
+  }
+
+
+
+}
