01f684384e2614f3c187aaffd5d19a373c50fb6a
ChristinaAdmin <ctinabye@outlook.com>
2025-03-13 13:21:52 +0100

    feat(DeckOfCards): add dealHand method

--- src/main/java/edu/ntnu/idi/idatt2003/cardgame/Model/DeckOfCards.java
+++ src/main/java/edu/ntnu/idi/idatt2003/cardgame/Model/DeckOfCards.java
@@ -9,7 +9,7 @@ import java.util.Random;
  *
  * @version 1.0
  */
-public class DeckOfCards {
+public class  DeckOfCards {
   private static final char[] suit = {'S', 'H', 'D', 'C'};
   private List<PlayingCard> cards;
 
@@ -35,10 +35,27 @@ public class DeckOfCards {
     return cards;
   }
 
-  public List<> dealHand(int n) {
-    if (cards.isEmpty() || n < 1 && n > 52) {
+  /**
+   * Deals a specified random amount of cards from the deck and returns the new hand of random cards.
+   *
+   * @param n cards to be dealt
+   * @return a hand of random cards
+   * @throws IllegalArgumentException when cards is empty or n is less than or greater than 52
+   */
+  public List<PlayingCard> dealHand(int n) {
+    if (cards.isEmpty() || n < 1 || n > 52) {
       throw new IllegalArgumentException("cards must be between 1 and 52");
     }
+
+    Random random = new Random();
+    List<PlayingCard> hand = new ArrayList<PlayingCard>();
+
+    for (int i = 0; i < n; i++) {
+      int index = random.nextInt(cards.size());
+      PlayingCard card = cards.remove(index);
+      hand.add(card);
+    }
+    return hand;
 
   }
 
