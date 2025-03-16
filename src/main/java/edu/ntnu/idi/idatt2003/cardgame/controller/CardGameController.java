682520b7b90276486824c48eaf649314173b9e50
ChristinaAdmin <ctinabye@outlook.com>
2025-03-16 14:43:17 +0100

    feat(CardGameController): add checkHandAction method

--- src/main/java/edu/ntnu/idi/idatt2003/cardgame/controller/CardGameController.java
+++ src/main/java/edu/ntnu/idi/idatt2003/cardgame/controller/CardGameController.java
@@ -2,21 +2,17 @@ package edu.ntnu.idi.idatt2003.cardgame.controller;
 
 import edu.ntnu.idi.idatt2003.cardgame.model.DeckOfCards;
 import edu.ntnu.idi.idatt2003.cardgame.model.HandOfCards;
-import edu.ntnu.idi.idatt2003.cardgame.model.PlayingCard;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * Controller class for managing the card game. It handles the actions related to dealing a hand of cards
+ * and checking the hand for specific conditions such as flush, sum of faces, presence of hearts, and the queen of spades.
+ */
 public class CardGameController {
-
 
     private DeckOfCards deck;
     private HandOfCards hand;
-
 
     private Label handDisplay;
     private TextArea flushDisplay;
@@ -24,7 +20,16 @@ public class CardGameController {
     private TextArea sumDisplay;
     private TextArea heartsDisplay;
 
-
+    /**
+     * Constructs a CardGameController with the specified deck and display components.
+     *
+     * @param deck the deck of cards to be used in the game
+     * @param handDisplay the label to display the hand of cards
+     * @param flushDisplay the text area to display if the hand has a flush
+     * @param queenDisplay the text area to display if the hand has the queen of spades
+     * @param sumDisplay the text area to display the sum of faces in the hand
+     * @param heartsDisplay the text area to display the hearts in the hand
+     */
     public CardGameController(DeckOfCards deck,
                               Label handDisplay,
                               TextArea flushDisplay,
@@ -37,20 +42,31 @@ public class CardGameController {
         this.queenDisplay = queenDisplay;
         this.sumDisplay = sumDisplay;
         this.heartsDisplay = heartsDisplay;
-
     }
 
-
+    /**
+     * Deals a hand of cards from the deck and updates the hand display.
+     */
     public void dealHandAction() {
         hand = new HandOfCards(deck.dealHand(5));
         handDisplay.setText(hand.toString());
     }
 
-
+    /**
+     * Checks the hand for specific conditions (sum of faces, presence of hearts, flush, and queen of spades)
+     * and updates the respective display components.
+     */
     public void checkHandAction() {
+        String sum = hand.sumFaces();
+        sumDisplay.setText(sum);
 
+        String heartCards = hand.showHearts();
+        heartsDisplay.setText(heartCards);
+
+        boolean flush = hand.hasFlush();
+        flushDisplay.setText(flush ? "Yes" : "No");
+
+        boolean queenOfSpades = hand.queenOfSpadesInHand();
+        queenDisplay.setText(queenOfSpades ? "Yes" : "No");
     }
-
-
-
 }
